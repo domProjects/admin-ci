5f5dac9b298747578ac091d0e2ed74e18c1424d9
emmanuel campait <ecampait@gmail.com>
2024-03-23 14:19:32 +0100

    test

---
+++ src/Helpers/hello_helper.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+if (! function_exists('hello'))
+{
+    /**
+     * 
+     */
+    function hello(): string
+    {
+        return '<h1>Hello</h1>';
+    }
+}
