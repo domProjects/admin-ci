df234380a22c8c95120e3e3938f643f9389692b4
emmanuel campait <ecampait@gmail.com>
2024-03-24 18:42:05 +0100

    add commands

---
+++ src/Commands/Exceptions/BadInputException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter Shield.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\AdminCI\Commands\Exceptions;
+
+use CodeIgniter\AdminCI\Exceptions\RuntimeException;
+
+class BadInputException extends RuntimeException
+{
+}
